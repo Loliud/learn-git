29afb2071edf310a13dc98749e1999fe964855b6
Loliud <info@loliud.tokyo>
2019-05-01 23:27:48 +0700

    let tom eat mickey

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 var Dog = require('./Dog');
 var Cat = require('./Cat');
-
+var Mouse = require('./Mouse');
 var newDog = new Dog('Wendy', 19);
 var tom = new Cat('Tom', 20);
-
+var mickey = new Mouse('Mickey', 20);
+tom.eat(mickey);
 newDog.eat(tom);
-
+console.log(newDog);
 
