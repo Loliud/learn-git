c5738ea5b174e136f2048a97b9f161a931a4a312
Loliud <info@loliud.tokyo>
2019-05-01 23:08:13 +0700

    add dog eat cat

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+
 var Dog = require('./Dog');
+var Cat = require('./Cat');
 
 var newDog = new Dog('Wendy', 19);
+var tom = new Cat('Tom', 20);
 
-newDog.sayHi();
+newDog.eat(tom);
+console.log(newDog);
+
